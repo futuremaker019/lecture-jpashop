d016c9e3b378581c5954055df4105b0b6e711a80
futuremaker <futuremaker019@gmail.com>
2022-03-16 00:14:09 +0900

    5.회원 도메인 개발/2.회원 서비스 개발

--- src/main/java/japbook/japshop/service/MemberService.java
+++ src/main/java/japbook/japshop/service/MemberService.java
@@ -17,11 +17,15 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     // 회원 가입
-    @Transactional()
+    @Transactional
     public Long join(Member member) {
 
         validateDuplicateMember(member);  // 중복 회원 검증
         memberRepository.save(member);
+
+        // em.persist하면 영속성 컨텍스트에 key:value 형태로 값이 들어가는데 key로 id를 사용한다.
+        // 동시에 Member 객체의 id 값이 GeneratedValue에 의해 id에 항상 값이 들어가게 된다.
+        // 그래서 id에 값이 있다는 것이 항상 보장된다.
         return member.getId();
     }
 
