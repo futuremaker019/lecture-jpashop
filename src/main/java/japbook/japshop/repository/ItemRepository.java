ec5d4333fac7c2536984a61ce375233cf4b34c64
futuremaker <futuremaker019@gmail.com>
2022-03-22 22:30:00 +0900

    상품 도메인 개발/상품 리포지토리 개발

--- src/main/java/japbook/japshop/repository/ItemRepository.java
+++ src/main/java/japbook/japshop/repository/ItemRepository.java
@@ -1,2 +1,34 @@
-package japbook.japshop.repository;public class ItemRepository {
+package japbook.japshop.repository;
+
+import japbook.japshop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            // id가 없으면 새로 생성
+            em.persist(item);
+        } else {
+            // id가 있으면 기존값을 강제로 수정
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
 }
